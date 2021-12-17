9278d380df28f46fc3d8c23baad005a56867e2ff
KaylaL989 <kayla.park.2014.kl@gmail.com>
2021-12-16 19:04:17 -0500

    cleaned up that mess

--- robotTools/launch/mimic.launch.py
+++ robotTools/launch/mimic.launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='turtlesim',
-            namespace='turtlesim1',
+            namespace='turtlesim8',
             executable='turtlesim_node',
             name='sim'
         ),
